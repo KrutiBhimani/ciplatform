bc9719604691692a32d03232079bba8383352c88
190540107014 <190540107014@darshan.ac.in>
2023-01-27 12:23:06 +0530

    new changes

--- mvc/Controller/homeController/policyController.php
+++ mvc/Controller/homeController/policyController.php
@@ -1,33 +1,39 @@
 <?php if (!isset($_SESSION['user_data'])) {
-?>
-    <script type="text/javascript">
-        window.location.href = 'login';
-    </script>
-<?php
-}
-$user_id = $_SESSION['user_data']->user_id;
-$where = [
-    'user_id' => $user_id
-];
-$selectData = $this->SelectData1('user', $where);
-$user = $selectData['Data'];
-$row = 4;
-$where = [
-    'slug' => 'policy'
-];
-$selectData = $this->SelectData12('cms_page', $where);
-$pages = $selectData['Data'];
-if (isset($_POST['contact'])) {
-    $insert_data = [
-        'user_id' => $user->user_id,
-        'subject' => $_POST['subject'],
-        'message' => $_POST['message']
+    $row = 4;
+    $where = [
+        'slug' => 'policy'
     ];
-    $insertEx = $this->InsertData('contact', $insert_data);
+    $selectData = $this->SelectData12('cms_page', $where);
+    $pages = $selectData['Data'];
+    echo '<!DOCTYPE html>';
+    include 'mvc/Views/home/header.php';
+    include 'mvc/Views/home/policy.php';
+    include 'mvc/Views/footer.php';
+} else {
+    $user_id = $_SESSION['user_data']->user_id;
+    $where = [
+        'user_id' => $user_id
+    ];
+    $selectData = $this->SelectData1('user', $where);
+    $user = $selectData['Data'];
+    $row = 4;
+    $where = [
+        'slug' => 'policy'
+    ];
+    $selectData = $this->SelectData12('cms_page', $where);
+    $pages = $selectData['Data'];
+    if (isset($_POST['contact'])) {
+        $insert_data = [
+            'user_id' => $user->user_id,
+            'subject' => $_POST['subject'],
+            'message' => $_POST['message']
+        ];
+        $insertEx = $this->InsertData('contact', $insert_data);
+    }
+    echo '<!DOCTYPE html>';
+    include 'mvc/Views/home/header.php';
+    include 'mvc/Views/home/header1.php';
+    include 'mvc/Views/home/policy.php';
+    include 'mvc/Views/home/footer.php';
 }
-echo '<!DOCTYPE html>';
-include 'mvc/Views/home/header.php';
-include 'mvc/Views/home/header1.php';
-include 'mvc/Views/home/policy.php';
-include 'mvc/Views/home/footer.php';
 ?>
